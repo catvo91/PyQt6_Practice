15bf30d17079fcc4f02731a5e918fda665b50d15
Catherine Vo <catvo91@gmail.com>
2023-06-07 18:56:14 -0700

    Second commit. Added a minimum size for the window and a simple push button.

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow #, QWidget
 
 #for command line args
 import sys
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("To Do Cat")
+        button = QPushButton("Meow")
+        
+        #sets size restraints on the window
+        #also setMax, setFixed
+        #can resize any widget
+        self.setMinimumSize(QSize(800, 600))
+
+
+        #sets central widget of the window
+        self.setCentralWidget(button)
+
+
 #only need 1 Qapp instance per app
 #pass sys.argv to allow command line arg for app
 # if you know you wont use command line args, QApplication[] works too
+
 app = QApplication(sys.argv)
 
 #create a Qt widget, which will be the window
 
-window = QWidget()
+#window = QWidget() #for making only a window
+#window = QPushButton("Kitty!") #creates a window wiht a button push
+window = MainWindow()
 window.show() #because windows are hidden by default
 
 #start event loop
