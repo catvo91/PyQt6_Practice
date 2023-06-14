8e865094eeb08ed4c590a32df2f733dd9f60e4a6
Catherine Vo <catvo91@gmail.com>
2023-06-13 22:48:16 -0700

    Third commit. Adde some additional functionality to the button.

--- app.py
+++ app.py
@@ -7,18 +7,38 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self._buttonChecked = True
         self.setWindowTitle("To Do Cat")
-        button = QPushButton("Meow")
+        self._button = QPushButton("Meow")
         
         #sets size restraints on the window
         #also setMax, setFixed
         #can resize any widget
         self.setMinimumSize(QSize(800, 600))
 
-
         #sets central widget of the window
-        self.setCentralWidget(button)
+        
+        self._button.setCheckable(True) #allows toggling of "Checked". Set to True = can toggle between T and F. Set to False = stays false.
+        self._button.released.connect(self.buttonClicked)
+        #self.button.released.connect(self.buttonReleased)
+        #self.button.clicked.connect(self.buttonToggled) #slot 2 for BUTTON
+        #self.button.setChecked(self._buttonChecked) #slot 1 for BUTTON
 
+        self.setCentralWidget(self._button)
+
+    def buttonClicked(self):
+        print("Button clicked!")
+        self._button.setText("Woof")
+        self._button.setEnabled(False) #deactivates the button
+        self.setWindowTitle("Sad Cat")
+
+    def buttonToggled(self, checked): #check refers to the T or F checked state of the button
+        self._buttonChecked = checked #this now changes the variable to T or F
+        print("Checked?", self._buttonChecked)
+
+    def buttonReleased(self):
+        self.buttonChecked = self.button.isChecked()
+        print("Release", self.buttonChecked)
 
 #only need 1 Qapp instance per app
 #pass sys.argv to allow command line arg for app
